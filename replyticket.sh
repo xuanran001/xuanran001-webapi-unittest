3e262bad56dd05d17b1d320050d7d70eca9fcdb5
Devin Chen <d3vin.chen@gmail.com>
2014-12-16 18:53:52 +0800

    impl ticket update

---
+++ replyticket.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+TICKETID=$1
+TICKETCOMMENT=$2
+
+IP="192.168.1.153"
+UPDATETICKETURL="https://raw.githubusercontent.com/sp-chenyang/xxutils/master/updateticket.py?$RANDOM"
+
+# 192.168.1.153
+UPDATETICKETPY="/tmp/updateticket.py"
+
+#
+# prepare working env
+#
+
+# load useful functions
+wget -q https://raw.githubusercontent.com/sp-chenyang/xxutils/master/xxutils.sh?$RANDOM -O $XXUTILS \
+    && chmod a+x $XXUTILS \
+    && . $XXUTILS
+
+# create random dir for me, on 192.168.2.21
+JDIR=$( gettmpdir "xuanran001_webapi_unittest" )
+cd "$JDIR"
+CMDFILE="$JDIR/updateticket_rcmd.sh"
+XXUTILS="$JDIR/xxutils.sh"
+
+# prepare remote shell script
+echo "" > $CMDFILE
+echo "curl $UPDATETICKETURL -o $UPDATETICKETPY" >> $CMDFILE
+
+#
+# begin to update a ticket
+#
+
+cmd="sudo python $UPDATETICKETPY"
+cmd="$cmd --id $TICKETID"
+cmd="$cmd --comment \"$TICKETCOMMENT\""
+cmd="$cmd --author \"ci\""
+echo $cmd >> $CMDFILE
+
+# just for debug
+cat $CMDFILE
+
+rcmdfile "$IP" "$CMDFILE"
