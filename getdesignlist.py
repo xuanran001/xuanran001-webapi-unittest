4f3f5160c26865d2d3a225b8cd164d58d4cddc7c
Devin Chen <d3vin.chen@gmail.com>
2014-12-17 16:58:37 +0800

    for test only

--- getdesignlist.py
+++ getdesignlist.py
@@ -199,7 +199,7 @@ class bug_Tests(unittest.TestCase):
 
     def test_ticket11285(self):
         xlog('test_ticket11285')
-        self.ticketid = "11285"
+        self.ticketid = "11547"
         self.msg = "\n测试`api/getdesignlist.html`接口，当参数`size=small`的时候，返回json中不应该包含水印图\n"
         for x in range(5):
             offset = random.randint(1, self.result_count)
